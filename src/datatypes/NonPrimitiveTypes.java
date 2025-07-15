4ecb5cdb2ed0a54b9d728b7f9c08e2956975a3bc
Gabriel Lacerda Lemos <gabriellemoslacerda@gmail.com>
2025-07-15 16:28:21 -0300

    Non Primitive Types

---
+++ src/datatypes/NonPrimitiveTypes.java
@@ -0,0 +1,15 @@
+package datatypes;
+
+public class NonPrimitiveTypes {
+    public static void main(String[] args) {
+
+        String name = "Gabriel Lacerda";
+        String nameUpperCase = name.toUpperCase();
+        System.out.println("Original name: " + name);
+        System.out.println("Uppercase name: " + nameUpperCase);
+
+        String city = "BELO HORIZONTE";
+        String cityLowerCase = city.toLowerCase();
+        System.out.println("City in lowercase: " + cityLowerCase);
+    }
+}
