af6f2f41c352ab7c0551624887e1bf40050a8460
Gabriel Lacerda Lemos <gabriellemoslacerda@gmail.com>
2025-07-15 16:04:43 -0300

    PrimitiveTypes

---
+++ src/datatypes/PrimitiveTypes.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        int age = 22;
+        double height = 1.68;
+        char initial = 'G';
+        boolean aliveOrDead = true;
+        Long bankBalance = 99999999L;
+    }
+}
