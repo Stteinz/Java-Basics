7a1bed904dca5c8627987338d114bb37c6f1bb92
Gabriel Lacerda Lemos <gabriellemoslacerda@gmail.com>
2025-07-20 20:50:44 -0300

    Loops

---
+++ src/loops/ForLoopExample.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+
+        for (int numberOfClones = 1; numberOfClones <= 10 ; numberOfClones++) {
+            System.out.println("Naruto made " +  numberOfClones + " clones");
+        }
+    }
+}
---
+++ src/loops/WhileLoopExample.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int numberOfClones = 0;
+        int maximumNumberOfClones = 10;
+
+        while (numberOfClones < maximumNumberOfClones){
+            numberOfClones++;
+            System.out.println("Naruto made " + numberOfClones + " clones");
+        }
+    }
+}
